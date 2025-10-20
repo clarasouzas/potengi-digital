a844bc737fb75a937a570c692266d5a7c44ab331
Elayne Fernandes <elaynefernandesoliveira@gmail.com>
2025-10-20 13:16:20 -0300

    mudança do nome do app

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include("usuarios.urls")),
+    path('', include("linkif.urls")),
     path('admin/', admin.site.urls),
 ]
---
+++ linkif/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "linkif/index.html")
+def login(request):
+    return render(request, "linkif/login.html")
+def cadastro_candidato(request):
+    return render(request, "linkif/cadastro_candidato.html")
+def cadastro_empresa(request):
+    return render(request, "linkif/cadastro_empresa.html")
+def vagas(request):
+    return render(request, "linkif/vagas.html")
+def perfil_cursos(request):
+    return render(request, "linkif/perfil_cursos.html")
