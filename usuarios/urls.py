c28627ecbb9a132baf547e73c3bb8e7e6f9a4780
Elayne Fernandes <elaynefernandesoliveira@gmail.com>
2025-10-20 12:00:45 -0300

    inicio da pag de login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index.html"),
-    path('', views.login, name="login.html"),
-    path('', views.cadastro_candidato, name="cadastro_candidato.html"),
-    path('', views.cadastro_empresa, name="cadastro_empresa.html"),
-    path('', views.vagas, name="vagas.html"),
-    path('', views.perfil_cursos, name="perfil_cursos.html"),
+    path('', views.index, name="index"),
+    path('login/', views.login, name="login"),
+    path('cadastro_empresa/', views.cadastro_candidato, name="cadastro_candidato"),
+    path('cadastro_empresa/', views.cadastro_empresa, name="cadastro_empresa"),
+    path('vagas/', views.vagas, name="vagas"),
+    path('perfil_cursos/', views.perfil_cursos, name="perfil_cursos"),
     
 ]
