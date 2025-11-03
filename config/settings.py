d01065c928d6d7f4378bddc48f4501aed00a8e85
Clara Souza <clarasouzacs07@gmail.com>
2025-11-03 11:04:58 -0300

    apps novos e estrutura pastas

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', 
+    'usuarios',
     'linkif',
+    'dashboard',
+    
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static", # arquivos static globais
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include("linkif.urls")),
-    path('admin/', admin.site.urls),
+    path("", include("linkif.urls")),
+    path("dashboard/", include("dashboard.urls")),
+    path("usuarios/", include("usuarios.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("admin/", admin.site.urls),
 ]
