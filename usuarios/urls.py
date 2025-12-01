845b5d8c634be605ed89df8739147b071033f5d1
saramonalisa <dev.saramonalisa@gmail.com>
2025-11-30 22:39:09 -0300

    feat: adiciona autenticação OAuth2 via API Google para o perfil de aluno

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -14,8 +14,7 @@ urlpatterns = [
     path("cadastro/aluno/", cadastro_aluno, name="cadastro_aluno"),
     path("cadastro/empresa/", cadastro_empresa, name="cadastro_empresa"),
 
-    # não é necessário enquanto usamos allauth
-    # path("login/", auth_views.LoginView.as_view(...), name="login"),
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 
     path("dashboard/", redirecionar_dashboard, name="redirecionar_dashboard"),
