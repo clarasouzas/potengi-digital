7431ccb7388b574db48414eb605e6b3705b3c6e7
saramonalisa <dev.saramonalisa@gmail.com>
2025-12-02 10:37:53 -0300

    fix: conserta conflitos com campo de username em create superuser

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,5 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+class UsuarioManager(BaseUserManager):
+    '''
+    Usuário admin do Django usando email no lugar de username
+    '''
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('O campo email é obrigatório'))
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_approved', True)
+        extra_fields.setdefault('tipo', 'coordenador')
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        
+        return self.create_user(email, password, **extra_fields)
 
 class Usuario(AbstractUser):
     TIPO_CHOICES = [
@@ -34,8 +63,27 @@ class Usuario(AbstractUser):
     # controle
     is_approved = models.BooleanField(default=False)
 
+    objects = UsuarioManager()
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
-
+    
     def __str__(self):
         return f"{self.email} ({self.tipo})"
+
+    def save(self, *args, **kwargs):
+        """
+        Garante que username seja único, mas não obrigatório
+        """
+        if not self.username:
+            base_username = self.email.split('@')[0]
+            username = base_username
+            counter = 1
+            
+            while Usuario.objects.filter(username=username).exists():
+                username = f"{base_username}{counter}"
+                counter += 1
+            
+            self.username = username
+        
+        super().save(*args, **kwargs)
